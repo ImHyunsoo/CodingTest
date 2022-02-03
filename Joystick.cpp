f00e7344e262fb4c9382b4563e88da2e9787abda
ImHyunsoo <lhs4772@gmail.com>
2022-02-03 18:17:18 +0900

    Update Joystick.cpp

---
+++ Joystick.cpp
@@ -0,0 +1,95 @@
+// 조이스틱
+#include <iostream>
+#include <random>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int udCnt = 0 ;
+
+int solution(string name) {
+    int answer = 0;
+   
+    // name = "JAN";
+    cout << "name: " << name << "  " << name.size() << endl; 
+
+    // 상하 처리  65 ~ 90
+    for (int i = 0; i < name.size(); i++) {
+        int num1 = int(name[i]) - int('A');
+        int num2 = abs(int(name[i]) - int('Z')-1);
+        udCnt += min(num1, num2);
+        // cout << name[i] << "  " << int(name[i]) << endl;
+        // cout << udCnt << endl; 
+    }
+    cout << endl;
+
+  
+    // 커서 이동 처리
+    // A인 곳에서  min(스트레이트, 리턴)
+    int move = name.size() - 1;
+    int left, right;
+    int distance;
+    int next_idx;
+    
+    for (int i = 0; i < name.size(); i++) {
+        next_idx = i + 1;
+        int tmp1 = 0;
+        int tmp2 = 0; 
+        if (name[i] == 'A') {
+            while (i + tmp2 >= 0 && name[i + tmp2] == 'A') {
+                tmp2--;
+            }
+            while (next_idx + tmp1 < name.size() && name[next_idx + tmp1] == 'A') { 
+                tmp1++; 
+            }
+            left = i + tmp2;
+            if(left < 0) left = 0;
+            right = name.size() - (next_idx + tmp1);
+            cout << "i: " << i << "  left: " << left << "   " << "right: " << right;
+            distance = min(left, right);
+            move = min(move, (left + right + distance));
+            cout <<  "  distance: " << distance << "   move: " << move << endl;
+        } 
+    }
+    cout << "udCnt: " << udCnt << "   move: " << move;
+    answer = udCnt + move;
+    cout << "   answer: " << answer << endl;
+
+    return answer;
+}
+
+int main() {
+    random_device rd;
+
+    // random_device 를 통해 난수 생성 엔진을 초기화 한다.
+    mt19937 gen(rd());
+    // mt19937 gen(123);
+
+    // 0 부터 99 까지 균등하게 나타나는 난수열을 생성하기 위해 균등 분포 정의.
+    // uniform_int_distribution<int> dis(0, 99);
+    uniform_int_distribution<int> dis(65, 90);       // 알파벳 A~Z 아스키코드 중에서 랜덤
+    uniform_int_distribution<int> strlen(1, 20);   // 문자열 갯수 1 ~ 20 중에서 랜덤 
+    
+    // cout << "A: " <<  int('A')  << endl;
+    // cout << "Z: " <<  int('Z')  << endl;
+
+    int n = strlen(gen);       // 문자열 갯수 받기
+    // cout << "n: " << n << "  "<< char(n) <<'\n';
+
+    string str = "";
+    for (int i = 0; i < n; i++) {
+        str += char(dis(gen));
+    }
+
+
+    auto start = std::chrono::steady_clock::now();
+
+    int result;
+    result = solution(str);
+
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";   // solution 함수  실행 시간
+
+    return 0;
+}
--- gymClothes.py
+++ gymClothes.py
@@ -1,102 +1,102 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# # 체육복
-import random
+# #!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+# # # 체육복
+# import random
 
 
 
-n = random.randint(2, 30)
-k = random.randint(1, n)
-m = random.randint(1, n)
-lost = random.sample(range(1, n+1), k)
-reserve = random.sample(range(1, n+1), m)
-print(n)
-print(lost, k)
-print(reserve, m)
-print()
+# n = random.randint(2, 30)
+# k = random.randint(1, n)
+# m = random.randint(1, n)
+# lost = random.sample(range(1, n+1), k)
+# reserve = random.sample(range(1, n+1), m)
+# print(n)
+# print(lost, k)
+# print(reserve, m)
+# print()
 
-########################################################################
-# lost_set = set(lost) - set(lost).intersection(set(reserve))
-# reserve_set = set(reserve) - set(lost).intersection(set(reserve))
-# print(lost_set)
-# print(reserve_set)
-# for i in list(reserve_set):
-#     if int(i)-1 in lost_set:
-#         lost_set -= {int(i)-1}
-#     elif int(i)+1 in lost_set:
-#         lost_set -= {int(i)+1}
-# print(n - len(lost_set))
+# ########################################################################
+# # lost_set = set(lost) - set(lost).intersection(set(reserve))
+# # reserve_set = set(reserve) - set(lost).intersection(set(reserve))
+# # print(lost_set)
+# # print(reserve_set)
+# # for i in list(reserve_set):
+# #     if int(i)-1 in lost_set:
+# #         lost_set -= {int(i)-1}
+# #     elif int(i)+1 in lost_set:
+# #         lost_set -= {int(i)+1}
+# # print(n - len(lost_set))
 
-################################################################33
+# ################################################################33
 
 
-survive = 0
-cloth_cnt = [1] * n
-print(cloth_cnt)
+# survive = 0
+# cloth_cnt = [1] * n
+# print(cloth_cnt)
 
-for i in lost:
-    cloth_cnt[i-1] -= 1
+# for i in lost:
+#     cloth_cnt[i-1] -= 1
 
-print(cloth_cnt)
+# print(cloth_cnt)
 
-for j in reserve:
-    cloth_cnt[j-1] += 1
+# for j in reserve:
+#     cloth_cnt[j-1] += 1
     
-print(cloth_cnt)
+# print(cloth_cnt)
 
-for i in lost:
-    if i in reserve:
-        survive += 1
+# for i in lost:
+#     if i in reserve:
+#         survive += 1
 
-# 나에게 빌려줄 수 있는 사람이 오직 한사람 뿐인 사람
+# # 나에게 빌려줄 수 있는 사람이 오직 한사람 뿐인 사람
 
-# 첫 번째랑 마지막 번째 학생 먼저 처리 
-if cloth_cnt[0] == 0 and cloth_cnt[1] == 2:
-    cloth_cnt[0] = 1
-    cloth_cnt[1] = 1
-    survive += 1
+# # 첫 번째랑 마지막 번째 학생 먼저 처리 
+# if cloth_cnt[0] == 0 and cloth_cnt[1] == 2:
+#     cloth_cnt[0] = 1
+#     cloth_cnt[1] = 1
+#     survive += 1
     
-if cloth_cnt[n-1] == 0 and cloth_cnt[n-2] == 2:
-    cloth_cnt[n-1] = 1
-    cloth_cnt[n-2] = 1
-    survive += 1
+# if cloth_cnt[n-1] == 0 and cloth_cnt[n-2] == 2:
+#     cloth_cnt[n-1] = 1
+#     cloth_cnt[n-2] = 1
+#     survive += 1
     
-print("첨과 끝 먼저 처리")    
-print(cloth_cnt)
+# print("첨과 끝 먼저 처리")    
+# print(cloth_cnt)
     
-temp = survive    
-while True:    
-    # 여유 없는 사람 처리
+# temp = survive    
+# while True:    
+#     # 여유 없는 사람 처리
     
-    for i in range(1, n-1):
-        if cloth_cnt[i] == 0:
-            if cloth_cnt[i-1] == 2 and cloth_cnt[i+1] == 2:
-                continue
-            elif cloth_cnt[i+1] == 2:
-                cloth_cnt[i] = 1
-                cloth_cnt[i+1] = 1
-                survive += 1
-            elif cloth_cnt[i-1] == 2:
-                cloth_cnt[i] = 1
-                cloth_cnt[i-1] = 1
-                survive += 1
-    if temp == survive:
-        break
-    else: 
-        temp = survive
+#     for i in range(1, n-1):
+#         if cloth_cnt[i] == 0:
+#             if cloth_cnt[i-1] == 2 and cloth_cnt[i+1] == 2:
+#                 continue
+#             elif cloth_cnt[i+1] == 2:
+#                 cloth_cnt[i] = 1
+#                 cloth_cnt[i+1] = 1
+#                 survive += 1
+#             elif cloth_cnt[i-1] == 2:
+#                 cloth_cnt[i] = 1
+#                 cloth_cnt[i-1] = 1
+#                 survive += 1
+#     if temp == survive:
+#         break
+#     else: 
+#         temp = survive
             
-print(cloth_cnt)         
+# print(cloth_cnt)         
 
-# 여유 있는 사람 처리
-for i in range(1, n-1):
-    if cloth_cnt[i] == 0:
-        if cloth_cnt[i-1] == 2 and cloth_cnt[i+1] == 2: 
-            survive += 1
-            # cloth_cnt[i] = 1
-            # cloth_cnt[i+1] = 1
+# # 여유 있는 사람 처리
+# for i in range(1, n-1):
+#     if cloth_cnt[i] == 0:
+#         if cloth_cnt[i-1] == 2 and cloth_cnt[i+1] == 2: 
+#             survive += 1
+#             # cloth_cnt[i] = 1
+#             # cloth_cnt[i+1] = 1
             
 
         
-answer = n - len(lost) + survive 
-print(answer, " = ", n, " - ", k, " + ", survive )
+# answer = n - len(lost) + survive 
+# print(answer, " = ", n, " - ", k, " + ", survive )
 
