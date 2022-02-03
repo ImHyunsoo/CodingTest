a3e02f3abadecbede428fcdbd60386f7dd161b75
ImHyunsoo <lhs4772@gmail.com>
2022-02-03 19:14:23 +0900

    Update 체육복 .cpp

--- Greedy-1-WorkoutClothes/WorkoutClothes.cpp
+++ Greedy-1-WorkoutClothes/WorkoutClothes.cpp
@@ -34,7 +34,7 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
     for(int i = 0; i < reserve.size(); i++) {
         _lost.erase(remove(_lost.begin(), _lost.end(), reserve[i]), _lost.end());
     }
-    cout << endl << "Overlap proc" << endl;
+    cout << endl << "overlapping treatment" << endl;
     cout << "lost: ";
     for(int i = 0; i< _lost.size(); i++) {
         cout << _lost[i] << ' '; 
@@ -46,7 +46,7 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
     cout << endl << endl;
 
 
-    //     if (*find(data_types.begin(), data_types.end(), element_to_check1) == element_to_check1) {
+    //     if (*find(data_types.begin(), data_types.end(), element_to_check1) == element_to_check1) 
 
     // 여벌있는 사람은 앞에서 부터 순차적으로 앞에 줄 사람 있으면 주고 없으면 뒷 사람 줄 사람 주자
     for(int i = 0; i < _reserve.size(); i++) {
@@ -83,8 +83,7 @@ vector<int> GenRandom(int n) {
     // uniform_int_distribution<int> dis(0, 99);
     uniform_int_distribution<int> dis(1, n);
     
-    // 중복되고 잇음
-
+    // 중복되지 않게 랜덤한 학생 뽑기
     int visited_test[n];
     memset(visited_test, 0, n*4);
     // for (int i = 0; i < n; i++) {
@@ -119,77 +118,61 @@ vector<int> GenRandom(int n) {
 
 int main() {
 
-  for(int x = 0; x < 20; x++){
-    int n;
-    vector<int> lost;
-    vector<int> reserve;
-    
+    for(int x = 0; x < 20; x++){           // 20번 테스트
+        int n;
+        vector<int> lost;
+        vector<int> reserve;
+        
 
-    random_device rd;
+        random_device rd;
 
-    // random_device 를 통해 난수 생성 엔진을 초기화 한다.
-    mt19937 gen(rd());
-    // mt19937 gen(123);
+        // random_device 를 통해 난수 생성 엔진을 초기화 한다.
+        mt19937 gen(rd());
+        // mt19937 gen(123);
 
-    // 0 부터 99 까지 균등하게 나타나는 난수열을 생성하기 위해 균등 분포 정의.
-    // uniform_int_distribution<int> dis(0, 99);
-    uniform_int_distribution<int> dis(2, 30);
-    
-    n = dis(gen);
-    // cout << "n: " << n <<'\n';
- ///////////////////////////////////////////////////////////////////////
+        // 0 부터 99 까지 균등하게 나타나는 난수열을 생성하기 위해 균등 분포 정의.
+        // uniform_int_distribution<int> dis(0, 99);
+        uniform_int_distribution<int> dis(2, 30);
+        
+        n = dis(gen);
+        // cout << "n: " << n <<'\n';
+    ///////////////////////////////////////////////////////////////////////
 
-     // 난수 기본 예제
-    // for (int i = 0; i < 5; i++) {
-    //     cout << "난수 : " << dis(gen) << endl;
-    
-    // }
-    
-    //////////////////////////////////////////////////////////
+        // 난수 기본 예제
+        // for (int i = 0; i < 5; i++) {
+        //     cout << "난수 : " << dis(gen) << endl;
+        
+        // }
+        
+        //////////////////////////////////////////////////////////
 
-       /// 중복되고 잇음
-    // int k = dis(gen);
-    // for (int i = 0; i < k; i++) {
-    //     lost.push_back(dis(gen));
-    //     // cout << "lost[" << i << "]: " << lost[i] << '\n';
-    // }
+        /// 중복되고 있음
+        // int k = dis(gen);
+        // for (int i = 0; i < k; i++) {
+        //     lost.push_back(dis(gen));
+        //     // cout << "lost[" << i << "]: " << lost[i] << '\n';
+        // }
 
-    // k = dis(gen);
-    // for (int i = 0; i < k; i++) {
-    //     reserve.push_back(dis(gen));
-    //     // cout << "reserve[" << i << "]: " << reserve[i] << '\n';
-    // }
+        // k = dis(gen);
+        // for (int i = 0; i < k; i++) {
+        //     reserve.push_back(dis(gen));
+        //     // cout << "reserve[" << i << "]: " << reserve[i] << '\n';
+        // }
 
-    //////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////
 
+        
+        lost = GenRandom(n);         // 중복 없이 체육복 잃어버린 학생
+        reserve = GenRandom(n);       // 중복 없이 체육복 여분있는 학생
 
-       /// 중복 x
-    // int k = dis(gen);
-    // for (int i = 0; i < k; i++) {
-    //     int num = dis(gen);
-    //     lost.push_back(dis(gen));
-    //     // cout << "lost[" << i << "]: " << lost[i] << '\n';
-    // }
-
-    // k = dis(gen);
-    // for (int i = 0; i < k; i++) {
-    //     reserve.push_back(dis(gen));
-    //     // cout << "reserve[" << i << "]: " << reserve[i] << '\n';
-    // }
-    
-    lost = GenRandom(n);
-    reserve = GenRandom(n);
-
-    sort(lost.begin(), lost.end());
-    sort(reserve.begin(), reserve.end());
-    
-    cout << endl;
-    int result = solution(n, lost, reserve);
-    cout << "result: " << result << '\n';
-    cout << endl << endl << endl;
-  }
-
-
+        sort(lost.begin(), lost.end());          // 번호 순대로 정렬
+        sort(reserve.begin(), reserve.end());
+        
+        cout << endl;
+        int result = solution(n, lost, reserve);
+        cout << "result: " << result << '\n';
+        cout << endl << endl << endl;
+    }
 
     return 0;
 }
