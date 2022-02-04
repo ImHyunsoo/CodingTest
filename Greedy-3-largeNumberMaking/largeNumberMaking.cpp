0256e8266500fa16d4b2531b1bcee4fc1899980b
ImHyunsoo <lhs4772@gmail.com>
2022-02-04 19:20:43 +0900

    Updatd code && comments [largeNumberMaking.cpp]

--- Greedy-3-largeNumberMaking/largeNumberMaking.cpp
+++ Greedy-3-largeNumberMaking/largeNumberMaking.cpp
@@ -9,32 +9,19 @@ using namespace std;
 string solution(string name, int k) {
     string answer = "";
    
-    // cout  << name.size() << ' ' << k << endl;
-    // name = "4177252841";
-    // k = 4;
-
-
     // stack 이용
     // 앞에서 부터 담고 뒤에 오는 숫자가 더 크면 앞에꺼랑 비교해서 크면 앞에꺼 버리기 
     // 비교해서 작으면  스택에 추가 
     //  스택이 비어있으면 숫자 추가
-
-    vector<int> my_stack;
-    my_stack.push_back(name[0] - '0');
+    answer.push_back(name[0]);
     for (int i = 1; i < name.size(); i++) {
-        while (my_stack.size() > 0 && k > 0 && my_stack.back() < name[i] -'0') {
-            my_stack.pop_back();
+        while (answer.size() > 0 && k > 0 && answer.back() < name[i]) {
+            answer.pop_back();
             k--;      
         } 
-        my_stack.push_back(name[i]-'0');
+        answer.push_back(name[i]);
     }
-
-  
-    for (int i = 0; i < my_stack.size()-k; i++) {
-        answer += my_stack[i] + '0';
-    }
-
-    // cout << "answer: " << answer << endl;
+    answer = answer.substr(0, answer.size()-k);    // 전체 문자열 길이에서 k개 만큼 뺀 문자열
     
     return answer;
 }
